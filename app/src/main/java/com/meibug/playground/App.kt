4d641d95ff1ef0e4a70b7281886304746cb889ce
Rick Xing <oreofish@gmail.com>
2016-06-29 11:24:43 +0800

    add timber log

--- app/src/main/java/com/meibug/playground/App.kt
+++ app/src/main/java/com/meibug/playground/App.kt
@@ -2,6 +2,7 @@ package com.meibug.playground
 
 import android.app.Application
 import org.jetbrains.anko.toast
+import timber.log.Timber
 
 /**
  * Created by xing on 16/6/23.
@@ -12,10 +13,15 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree());
+        } else {
+            Timber.plant(CrashReportingTree());
+        }
     }
 
     companion object {
         lateinit var instance: App
     }
-
 }
